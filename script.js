8e862ad4299a3735e36224a9638aa5b06f57e7ba
tacullarr-oss <tacullarr@gmail.com>
2026-09-12 17:29:10 +0700

    Update modal popup to use portfolio-desc class for full descriptions

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.querySelectorAll(".open-detail").forEach((button) => {
         
         const box = this.closest(".portfolio-box");
         const title = box.querySelector(".portfolio-layer h4").innerText;
-        const description = box.querySelector(".portfolio-layer p").innerText;
+        const description = box.querySelector(".portfolio-desc").innerText;
 
         modalProjectTitle.innerText = title;
         modalProjectDesc.innerText = description;
